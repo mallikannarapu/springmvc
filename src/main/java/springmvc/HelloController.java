d526df8c519c98099702dec7ddfe7b5261c816cc
Mallika <mallikaannarapu@gmail.com>
2019-10-10 11:29:39 -0700

    Initial Commit

---
+++ src/main/java/springmvc/HelloController.java
@@ -0,0 +1,19 @@
+package springmvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class HelloController extends AbstractController{
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		ModelAndView model=new ModelAndView("Hellopage");
+		model.addObject("name", "MALLIKA");
+		return model;
+	}
+
+}
